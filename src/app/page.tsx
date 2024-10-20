ad10a9afb1684b420d02ae2138526261489cd73e
Naveen shaikh <shaikhazan0@gmail.com>
2024-10-20 10:24:50 -0700

    my assignment no 01

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,25 @@
 import Link from "next/link";
-
-
-
+import styles from './page.module.css';
 const homepage =()=>{
     return(
-        <>
-            <h1 >HOME PAGE </h1>
-            <p>You are on home page.</p>
+        <div className={styles.body}>
+        <nav className={styles.nav}>
+            <h1 className={styles.h1}>HOME PAGE </h1>
+            </nav>
+
+            <p className={styles.p}>You are on home page.</p>
         
-        <nav>
-   <ul>
+      <div>  
+   <ul className={styles.pages}>
     <li ><Link href ="/about"> About Us </Link></li>
     <li  ><Link href="/contact">Contact us</Link></li>
+    <li> <Link href="/services"> Services</Link></li>
    </ul>
-        </nav>
-        </>
+   </div>
+        
+        </div>
+        
+        
     );
 };
 export default homepage;

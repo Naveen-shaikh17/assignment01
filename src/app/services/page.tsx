b620ef11c59154aa7a0bd344f2a0f53536f49541
Naveen shaikh <shaikhazan0@gmail.com>
2024-10-20 04:24:53 -0700

    page.tsx

---
+++ src/app/services/page.tsx
@@ -0,0 +1,9 @@
+const services=()=>{
+    return(
+        <div>
+            <nav><h1>SERVICES</h1></nav>
+            <p>You are on services page</p>
+        </div>
+    )
+}
+export default services;
